03f00cde20fb5e95ff79a52ecc3e0345f877eb50
Niyaz <nieandevar@mail.ru>
2021-05-11 20:18:49 +0300

    bad

---
+++ ControlWork/ControlWork/Airplane.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ControlWork
+{
+    public class Airplane
+    {
+        public int price;
+        public int NumOfStopping;
+        public int PriceOfStopping;
+        public int FuelPrice;
+        public int ConsumptionPer100Km;
+        public int PriceForRent;
+        public int Tax;
+        public int TaxMultiplier;
+        public int Legth;
+        
+        public void CalculateFinalPrice()
+        {
+            int PriceFor100km = FuelPrice * ConsumptionPer100Km;
+            int PriceForStopping = NumOfStopping * PriceOfStopping;
+            int FullTax = Tax * TaxMultiplier;
+            int FullPrice = PriceFor100km + PriceForStopping + PriceForRent - FullTax;
+            Console.WriteLine($"Full Price for {Legth} = {FullPrice} ");
+        }
+    }
+}
---
+++ ControlWork/ControlWork/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ControlWork
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
